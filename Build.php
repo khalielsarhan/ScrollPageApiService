ecc0b47921178654adf68d84eadaa7c6b5d6ca9c
khalielsarhan <nirothoughtz_uwk@yahoo.co.uk>
2021-01-19 21:25:20 +0200

    Add files via upload

---
+++ Build.php
@@ -0,0 +1,21 @@
+﻿<?php
+include ('conn.php');
+$myarray = array();
+$myrow = array();
+
+
+$query = "SELECT * FROM `blog_posts` ORDER BY id ASC LIMIT 1 OFFSET 2";
+$result = mysqli->query($query); 
+
+
+while($row = $result->fetch_array()){
+    $myrow[]= array(
+        "id"=$row['id'],
+        "content"=$row['content']
+        "date"=$row['date']
+);
+}
+
+$myarray['content'] = $myrow;
+
+echo json_encode($myarray);
